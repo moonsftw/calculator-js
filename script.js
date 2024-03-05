e241bb51a10e9770beaa2d47fe5a84a4bdce29fa
Facundo Luna <nfacundoluna@gmail.com>
2024-03-05 17:34:41 -0300

    script: funciones terminadas

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //DOM elements
-  //1. Display
+//1. Display
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
 
-  //2. Buttons 
+//2. Buttons 
 const numberButtons = document.querySelectorAll('[data-number]');
 const operatorButtons = document.querySelectorAll('[data-operator]');
 const allClearButton = document.querySelector('[data-all-clear]');
@@ -12,47 +12,90 @@ const equalsButton = document.querySelector('[data-equals]');
 
 //Constructor 
 
-class Calculator{
+class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
     this.previousOperandTextElement = previousOperandTextElement;
     this.currentOperandTextElement = currentOperandTextElement;
 
-    this.currentOperand = "";
-    this.previousOperand = ""; 
-    this.operation = undefined;
+    this.allClear();
   }
 
   allClear() {
-
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
   }
 
   deleteDigit() {
-    this.currentOperand = this.currentOperand.slice(0,-1);
+    this.currentOperand = this.currentOperand.slice(0, -1);
   }
 
   appendDigit(digit) {
+    // rejects inputs that render the operand invalid (multiple decimal pointer)
     if (digit === "." && this.currentOperand.includes(".")) return;
-
-    if( digit === '0' && this.currentOperand === '0') return;
+    // if number is zero do not allow multiple initial zeros
+    if (digit === '0' && this.currentOperand === '0') return;
+    // if digit is .  and there are no prior digits, add a zero
+    if (digit === '0' && this.currentOperand === '') this.currentOperand = '0';
 
     this.currentOperand += digit;
   }
 
-  selectOperator() {
+  selectOperation(operation) {
 
+    if(this.currentOperand === '' && operation === '-'){
+      this.appendDigit(operation);
+      return;
+    }
+    if (this.currentOperand === '') return;
+    if (this.previousOperand !== '') this.calculate();
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = '';
   }
 
   calculate() {
+    let result;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
 
+    if (isNaN(prev) || isNaN(current)) return;
+
+    console.log(`prev: ${prev} current: ${current}`)
+
+    switch (this.operation) {
+      case '+':
+        result = prev + current;
+        break;
+      case '-':
+        result = prev - current; 
+        break;
+      case '*':
+        result = prev * current;
+        break;
+      case '÷':
+        result = prev / current
+        break;
+    
+      default:
+        return;
+    }
+    this.currentOperand = result.toString();
+    this.operation = undefined; 
+    this.previousOperand = '';
   }
 
   updateDisplay() {
     this.currentOperandTextElement.innerText = this.currentOperand;
+
+    if (this.operation !== undefined) {
+      this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = '';
+    }
   }
 
-  getOperandFromDisplay() {
 
-  }
 } /* class Calculator */
 
 
@@ -70,9 +113,21 @@ deleteButton.addEventListener('click', () => {
   calculator.updateDisplay();
 })
 
+equalsButton.addEventListener('click', () => {
+  calculator.calculate();
+  calculator.updateDisplay();
+})
+
 numberButtons.forEach(button => {
   button.addEventListener("click", () => {
     calculator.appendDigit(button.innerText);
+    calculator.updateDisplay();
+  })
+})
+
+operatorButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.selectOperation(button.innerText);
     calculator.updateDisplay();
   })
 })
